88aa7893a15f2b9812e7db21d444d46622f4fb3f
Kshitij Mudgal <iamkshitij@live.com>
2017-09-06 08:25:48 +0530

    Android transparent UI

---
+++ setTransparentUI.java
@@ -0,0 +1,11 @@
+public void getTransparentUI(){
+
+        getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+    }
